69468f625e82f35f8b3dc7c8f6699dbdfabca981
Daniels1610 <danielagraz1610@hotmail.com>
2024-04-06 15:38:03 -0700

    Add PeSTO evaluation for pawns

--- src/My Bot/MyBot.cs
+++ src/My Bot/MyBot.cs
@@ -1,5 +1,13 @@
 using System;
+using Raylib_cs;
+using System.Linq;
 using ChessChallenge.API;
+using static PieceSquareTables;
+using System.Runtime.InteropServices;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
+
 
 // PinChess Bot Implementation
 public class MyBot : IChessBot
@@ -11,30 +19,95 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        foreach (Move move in moves )
+
+        Move[] botMoves = board.GetLegalMoves();
+        // Gets each move and displays PieceType and Move's start and end square
+        Console.WriteLine("BOT MOVES");
+        foreach (Move move in board.GetLegalMoves())
         {
-            Console.WriteLine("Move: {0} - {1}", move.MovePieceType, move);
+            // Console.WriteLine("Move: {0} - {1}", move.MovePieceType, move);
+            if (move.MovePieceType.Equals(PieceType.Pawn)) {
+                Console.WriteLine("TARGET SQUARE {0}: FILE {1} | RANK {2}", move, move.TargetSquare.File, move.TargetSquare.Rank);
+                Console.WriteLine("PAWN POSITION EVAL: {0}", PestoEval(board, move, board.IsWhiteToMove));
+            }
         }
-        Console.WriteLine("Evaluation: {0}", Evaluate(board));
+        Console.WriteLine("MOVE PLAYED: {0} - {1}", botMoves[0].MovePieceType, botMoves[0]);
         
-        return moves[0];
+        board.TrySkipTurn();
+        Console.WriteLine("IsWhiteToMove: {0}", board.IsWhiteToMove);
+        Console.WriteLine("HUMAN MOVES");
+        foreach (Move move in board.GetLegalMoves())
+        {
+            // Console.WriteLine("Move: {0} - {1}", move.MovePieceType, move);
+            if (move.MovePieceType.Equals(PieceType.Pawn)) {
+                Console.WriteLine("TARGET SQUARE {0}: FILE {1} | RANK {2}", move, move.TargetSquare.File, move.TargetSquare.Rank);
+                Console.WriteLine("PAWN POSITION EVAL: {0}", PestoEval(board, move, board.IsWhiteToMove));
+            }
+        }
+        board.UndoSkipTurn();
+
+        // int bestMove = Search(board, depth);
+        // Console.WriteLine("BestEvalMove: {0}", bestMove);
+        return botMoves[0];
+    }
+
+    // Evaluation function should check for:
+    //   - Material Balance for both Black and White players
+    //   - Developed Pieces (Center Squares) PeSTO's Evaluation Function
+    //   - King safety
+    //   - Pawn Structure
+    //   - Tactics (Gambits, Pins, Forks, Skewers)
+
+    int Search(Board board, int depth) 
+    {
+        if (depth == 0) {
+            return Evaluate(board);
+        }
+        Move[] moves = board.GetLegalMoves();
+        if (moves.Length == 0) {
+            if (board.IsInCheck()) {
+                return int.MinValue;
+            }
+            return 0;
+        }
+
+        int bestEvaluation = int.MinValue;
+        foreach (Move move in moves) {
+            board.MakeMove(move);
+            int evaluation = - Search(board, depth-1);
+            bestEvaluation = Math.Max(evaluation, bestEvaluation);
+            board.UndoMove(move);
+        }
+        return bestEvaluation;
     }
 
     int Evaluate(Board board)
     {
-        int whiteEval = CountMaterial(board);
-        int blackEval = CountMaterial(board);
+        int whiteMaterial = CountMaterial(board, true);
+        int blackMaterial = CountMaterial(board, false) * -1;
 
-        int evaluation = whiteEval - blackEval;
+        int evaluation = whiteMaterial + blackMaterial;
+        Console.WriteLine("Evaluation: {0}", evaluation);
 
-        int perspective = board.IsWhiteToMove ? 1 : -1;
-        return evaluation * perspective;
+        return evaluation;
     }
 
-    int CountMaterial(Board board)
+    private int PestoEval(Board board, Move move, bool player)
+		{
+			int eval = 0;
+			if (move.MovePieceType.Equals(PieceType.Pawn)){
+                if (player) {eval += PawnTable[PawnTable.GetUpperBound(0) + move.TargetSquare.Rank * -1, move.TargetSquare.File];}
+                else {eval += PawnTable[move.TargetSquare.Rank, move.TargetSquare.File] * -1;}
+			}
+			else if (move.MovePieceType.Equals(PieceType.Knight)){
+				eval += KnightTable[KnightTable.GetUpperBound(0) + move.TargetSquare.Rank * -1, move.TargetSquare.File];
+			}
+			return eval;
+	}
+
+    int CountMaterial(Board board, bool player)
     {
-        int material = 0; bool player = board.IsWhiteToMove;
+        int material = 0;
         material += board.GetPieceList(PieceType.Pawn, player).Count * pieceValues[1];
         material += board.GetPieceList(PieceType.Knight, player).Count * pieceValues[2];
         material += board.GetPieceList(PieceType.Bishop, player).Count * pieceValues[3];
