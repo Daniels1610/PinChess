d446eea4fd030625567aa4fbfcab2167bc7ba90e
Daniels1610 <danielagraz1610@hotmail.com>
2024-05-27 15:19:07 -0700

    Add Metaweights file and modify .gitignore

---
+++ src/My Bot/Metaweights.cs
@@ -0,0 +1,9 @@
+public static class Metaweights
+{
+    public static float endgameWeight = 0F;
+    public static bool endgamePawn = false;
+    public static bool endgameKnight = false;
+    public static bool endgameBishop = false;
+    public static bool endgameRook = false;
+    public static bool endgameQueen = false;
+}
